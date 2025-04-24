cfe2114eda89c13382bfee93f2a580e08ddf1146
aravindsuri <49809361+aravindsuri@users.noreply.github.com>
2025-04-24 12:58:56 +0200

    Committing 3 items from workspace 22983cb6-3a29-4ad2-856e-d35c1222e126

---
+++ Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,55 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "d84130d3-ecf4-43ea-a50a-f279da943b17",
+# META       "default_lakehouse_name": "lh_bicycles",
+# META       "default_lakehouse_workspace_id": "22983cb6-3a29-4ad2-856e-d35c1222e126",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "d84130d3-ecf4-43ea-a50a-f279da943b17"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# 
+# #### Run the cell below to install the required packages for Copilot
+
+
+# CELL ********************
+
+
+#Run this cell to install the required packages for Copilot
+%load_ext dscopilot_installer
+%activate_dscopilot
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
